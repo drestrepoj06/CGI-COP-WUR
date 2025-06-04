ad8e78a36d10e932fd4e432acaa78f7458cc480b
Thijs Vons <t.a.vons@student.utwente.nl>
2025-06-04 12:04:31 +0200

    Initialised Streamlit package

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,1 +1,9 @@
-print("Hello Frontend World!")
+import streamlit as st
+
+st.title("Hello from Streamlit in Docker!")
+st.write("This is running inside a Docker container.")
+
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
+
+st.write("Test")
