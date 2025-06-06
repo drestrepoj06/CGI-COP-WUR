062ca28fda616ef5bb43ecd23796ac8dc21a12ef
fyan <frankxiaoluyan@gmail.com>
2025-06-06 09:57:11 +0200

    solve conflict

---
+++ backend/utils/format_producer.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+# 52.12127432102285, 5.035082300971591
+# Standplaats Maarssen (Floraweg 17, Maarssen)
+# 52.10894869388968, 5.114417475677879
+# Standplaats Vader Rijndreef (Vader Rijndreef 1, Utrecht)
+# 52.08097319448016, 5.137714973657466
+# Standplaats Diakonessenhuis (Burgemeester Fockema Andreaelaan 90, Utrecht)
+
+
+
