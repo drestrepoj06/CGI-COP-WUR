73ab25965ba31700d76f50b70649a106dfab022b
drestrepoj06 <132402935+drestrepoj06@users.noreply.github.com>
2025-06-27 09:31:13 +0200

    Alerts always there

--- src/utils/web_dashboard.py
+++ src/utils/web_dashboard.py
@@ -1,25 +1,32 @@
 import streamlit as st
-import time
 from utils.web_map import render_map_section
-from utils.web_controls import render_train_controls, display_ambulance_alerts, display_train_alerts
-from utils.web_ambulance import display_ambulance_data
+from utils.web_controls import render_train_controls, display_incident_summary
+from utils.web_ambulance import display_ambulance_availabitity_data
+from utils.join_query import display_rescue_progress_auto
+
+from utils.web_controls import display_ambulance_alerts, display_train_alerts
+
+import redis
+
+
+client = redis.Redis(host="tile38", port=9851, decode_responses=True)
+
 
 def render_dashboard():
-
-    # Create columns for layout
-    col = st.columns((2.7, 4.8, 1), gap="small")
+    col = st.columns((2, 5.5, 1), gap="small")
 
     with col[0]:
-        # Render the ambulance data and control buttons
-        ambulance_data = display_ambulance_data()
-
-        # Button to manually refresh the alerts
-        if st.button("Refresh Alerts"):
-            display_ambulance_alerts()
-            display_train_alerts()
+        st.markdown("#### Incident Summary")
+        display_incident_summary()
+        display_rescue_progress_auto(client)
 
     with col[1]:
         render_map_section()
+        display_ambulance_availabitity_data()
 
     with col[2]:
         render_train_controls()
+
+        # Button to manually refresh the alerts
+        display_ambulance_alerts()
+        display_train_alerts()
